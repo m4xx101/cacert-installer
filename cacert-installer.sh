ed6b08fcbcda5fc24f2dd6ec32170714ad693d66
m҉4҉x҉X҉.҉!҉ <m4xx101101@gmail.com>
2020-09-04 22:18:43 +0530

    Update cacert-installer.sh

--- cacert-installer.sh
+++ cacert-installer.sh
@@ -6,7 +6,6 @@ hash = openssl x509 -inform PEM -subject_hash_old -in burp.pem |head -1
 mv burp.pem $hash.0
 adb root
 adb remount
-adb shell
 adb push $hash.0 /system/etc/security/cacerts
 adb -c "chmod 644 /system/etc/security/cacerts/$hash.0"
 reboot
